488fbaa1ad574d6b12eb3f83527eae6fb6f45adf
MatheusBarbosaa <matheussbarbosa06@gmail.com>
2024-06-21 11:42:41 -0300

    ok

---
+++ public/html/app.js
@@ -0,0 +1,6 @@
+document.getElementById('tecnologia-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que o link abra uma nova página
+
+    // Mostra o exemplo do Bootstrap
+    document.getElementById('bootstrap-example').style.display = 'block';
+});

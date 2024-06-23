56367cbb4657716aafad59abb24e3041d6139336
MatheusBarbosaa <matheussbarbosa06@gmail.com>
2024-06-23 01:25:43 -0300

    incluindo mais cursos

--- public/html/app.js
+++ public/html/app.js
@@ -3,11 +3,6 @@ document.getElementById('arquitetura-e-design-link').addEventListener('click', f
     showOnlySelectedArea('bootstrap-example-arquitetura');
 });
 
-document.getElementById('tecnologia-link').addEventListener('click', function (event) {
-    event.preventDefault();
-    showOnlySelectedArea('bootstrap-example');
-});
-
 document.getElementById('ciencias-juridicas-link').addEventListener('click', function (event) {
     event.preventDefault();
     showOnlySelectedArea('bootstrap-example-juridico');
@@ -27,6 +22,13 @@ document.getElementById('engenharia-link').addEventListener('click', function (e
     event.preventDefault();
     showOnlySelectedArea('bootstrap-example-engenharia');
 });
+/*
+document.getElementById('tecnologia-link').addEventListener('click', function (event) {
+    event.preventDefault();
+    showOnlySelectedArea('bootstrap-example');
+});
+
+
 
 document.getElementById('negocios-link').addEventListener('click', function (event) {
     event.preventDefault();
@@ -42,6 +44,7 @@ document.getElementById('todos-link').addEventListener('click', function (event)
     event.preventDefault();
     showAllAreas();
 });
+*/
 
 function showOnlySelectedArea(areaId) {
     const areaContents = document.querySelectorAll('.area-content');
